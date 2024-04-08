a5d56f5e0191e49f99fe59e4857aeff75e5d2d5c
Adam-CCC <79598536+Adam-CCC@users.noreply.github.com>
2024-04-09 00:14:55 +0300

    cycle added product and get data with server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import {products as data} from './data/products';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
+import { ProductService } from './service/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'product store';
-  products: IProduct[] = data
+  products: IProduct[] = []
+
+  constructor(private productService: ProductService){}
+
+  ngOnInit(): void {
+    this.productService.getAllProduct().subscribe(productItem => {
+        this.products = productItem;
+    })
+  }
 }
